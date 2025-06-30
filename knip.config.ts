3f705871fbd1554e15ddeb7a01b0fae8f1c1b31b
kazuya kawaguchi <kawakazu80@gmail.com>
2025-06-30 14:32:53 +0900

    fix: relative entries

--- knip.config.ts
+++ knip.config.ts
@@ -1,14 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  entry: [
-    'src/index.ts',
-    'src/tsdown.ts',
-    'src/unbuild.ts',
-    'eslint.config.ts',
-    'tsdown.config.ts',
-    'playground/tsdown/tsdown.config.ts'
-  ],
-
+  // entry: [],
   ignoreDependencies: ['lint-staged', 'tsdown', 'unbuild']
 } satisfies KnipConfig
