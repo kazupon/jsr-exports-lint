472d76fc36caafb0a3f2ddc4ceb7d27520f0e615
kazuya kawaguchi <kawakazu80@gmail.com>
2025-12-30 21:31:26 +0900

    fix: bump tsdown

--- playground/tsdown/tsdown.config.ts
+++ playground/tsdown/tsdown.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts', './src/array/index.ts', './src/utils.ts'],
+  clean: true,
+  outDir: 'dist',
+  dts: true,
+  fixedExtension: false,
   hooks: {
     'build:done': lintJsrExports()
   }
